5d4654f64ab9c870972168a5a6896654b4cc9fe6
Felipe Llerena <llerenafelipe7@gmail.com>
2019-03-12 18:55:44 -0500

    READ and DELETE Action

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php include('includes/header.php'); ?>
 
   <div class="container p-4">
+    <div class="row">
     <div class="col-md-4">
       <?php if (isset($_SESSION['message'])) { ?>
           <div class="alert alert-<?= $_SESSION['message_type'] ?> alert-dismissible fade show" role="alert">
@@ -25,7 +26,32 @@
       </div>
     </div>
     <div class="col-md-8">
-      
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Created At</th>
+            <th colspan="2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+              $query = "SELECT * FROM tasks";
+              $result_tasks = mysqli_query($conn, $query);
+
+              while ($row = mysqli_fetch_array($result_tasks)) { ?>
+                <tr>
+                  <td><?= $row['title']; ?></td>
+                  <td><?= $row['description']; ?></td>
+                  <td><?= $row['created_at']; ?></td>
+                  <td><a href="edit.php?id=<?= $row['id']; ?>" class="btn btn-warning">Edit</a></td>
+                  <td><a href="delete_task.php?id=<?= $row['id']; ?>" class="btn btn-danger">Delete</a></td>
+                </tr>
+              <?php } ?>
+        </tbody>
+      </table>
+    </div>
     </div>
   </div>
 
